637100a6e9db669d2d4968c7f00050f157073879
Kim Nguyen <kimnivore@gmail.com>
2021-12-03 10:20:03 -0800

    started cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,4 @@
-
+import axios from 'axios';
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -18,9 +18,35 @@
   // </div>
   //
   const Card = (article) => {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    headline.textContent = article.headline;
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+    img.src = article.authorPhoto;
+    authorName.textContent = article.authorName;
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(authorName);
+    imgContainer.appendChild(img);
+
+    card.addEventListener('click', () => {
+      console.log(headline.textContent);
+    })
+    
+    return card;
 }
 
-const cardAppender = (selector) => {
+
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
@@ -29,6 +55,21 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  // const parent = document.querySelector(selector);
+  //   parent.appendChild(Card());
+  
+
+  const cardAppender = (selector) => {
+    // for(let i = 0; i < articles.length; i++) {
+    axios.get(`http://localhost:5000/api/articles`)
+  .then(response => {
+    const newCard = Card(response.data.articles);
+    document.querySelector(selector).appendChild(newCard);
+    console.log(response);
+  }).catch(error => {
+    console.error(error);
+  }).finally(() => console.log('done'));
 }
+
 
 export { Card, cardAppender }
