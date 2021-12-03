fe58e2602350ede41ca0aa711e01547c86dd5633
Kim Nguyen <kimnivore@gmail.com>
2021-12-02 17:57:23 -0800

    completed header task 1 and 2

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,15 @@
+  // TASK 1
+  // ---------------------
+  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+  //
+  //  <div class="header">
+  //    <span class="date">{ date }</span>
+  //    <h1>{ title }</h1>
+  //    <span class="temp">{ temp }</span>
+  //  </div>
+  //
 const Header = (title, date, temp) => {
   const header = document.createElement('div');
   const dates = document.createElement('span');
@@ -17,33 +29,20 @@ const Header = (title, date, temp) => {
 
   return header;
 }
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
 
- 
-
-
-const headerAppender = (selector) => {
-  const parent = document.querySelector(selector);
-  parent.appendChild(Header(title, date, temp))
-
-}
-  // TASK 2
+ // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+ 
+
+const headerAppender = (selector) => {
+  const parent = document.querySelector(selector);
+  parent.appendChild(Header());
+}
+ 
  
 
 export { Header, headerAppender }
